0158ad43d1525a5f39174254d4cd7aac9495697f
Franz Aguillon <104345472+AwertzHVD@users.noreply.github.com>
2024-01-23 09:33:22 +0100

    MVC_Tutorial

---
+++ Workspace/BS2WebApp/Controllers/SchoolController.cs
@@ -0,0 +1,55 @@
+﻿using BS2WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BS2WebApp.Controllers
+{
+    public class SchoolController : Controller
+    {
+
+        public IActionResult GetSchool()
+        {
+            SchoolModel bsLinz2 = new SchoolModel()
+            {
+                Id = 401075,
+                Name = "BS Linz 2"
+            };
+
+            SchoolModel bsLinz4 = new SchoolModel()
+            {
+                Id = 401076,
+                Name = "Bs Linz 4"
+            };
+
+            List<SchoolModel> schoolList = new List<SchoolModel> { bsLinz2, bsLinz4 };
+
+            return View(schoolList);
+
+        }
+
+        public IActionResult EditSchool(int Id)
+        {
+            SchoolModel bsLinz2 = new SchoolModel()
+            {
+                Id = 401075,
+                Name = "BS Linz 2"
+            };
+
+            SchoolModel bsLinz4 = new SchoolModel()
+            {
+                Id = 401076,
+                Name = "Bs Linz 4"
+            };
+
+            List<SchoolModel> schoolList = new List<SchoolModel> { bsLinz2, bsLinz4 };
+
+            return View(schoolList.Where(x => x.Id == Id).FirstOrDefault());
+
+        }
+
+        [HttpPost]
+        public IActionResult EditSchool(SchoolModel model)
+        {
+            return View(model);
+        }
+    }
+}
